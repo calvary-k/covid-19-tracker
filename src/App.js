eaa9803e799f9c1344d7799af7706d8e587148d0
calvary-k <karamoy.calvary@gmail.com>
2021-03-13 21:34:07 +0800

    feat: :sparkles: add Cards component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+import { fetchCaseData, fetchVaccineData } from './api';
+import { Cards } from './components';
 import styles from './App.module.css';
 
 class App extends React.Component {
+  state = {
+    caseData: {},
+    vaccineData: {},
+    province: '',
+  };
+
+  async componentDidMount() {
+    const caseData = await fetchCaseData();
+    const vaccineData = await fetchVaccineData();
+
+    this.setState({ caseData, vaccineData });
+  }
+
   render() {
+    const { caseData, vaccineData } = this.state;
+
     return (
       <div className={styles.container}>
-        <h1>App</h1>
+        <Cards caseData={caseData} vaccineData={vaccineData}/>
       </div>
     );
   }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import CardComponent from './Card/Card';
+
+const Cards = ({ caseData: { confirmed, recovered, deaths, lastUpdate: caseLastUpdate },
+  vaccineData: { vaccine, lastUpdate: vaccineLastUpdate }
+}) => {
+  if(!confirmed) {
+    return 'Loading...';
+  }
+
+  return (
+    <div>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          cardTitle="Infected"
+          value={confirmed}
+          lastUpdate={caseLastUpdate}
+          cardSubtitle="Number of active cases of COVID-19"
+        />
+        <CardComponent
+          cardTitle="Recovered"
+          value={recovered}
+          lastUpdate={caseLastUpdate}
+          cardSubtitle="Number of recoveries from COVID-19"
+        />
+        <CardComponent
+          cardTitle="Deaths"
+          value={deaths}
+          lastUpdate={caseLastUpdate}
+          cardSubtitle="Number of deaths caused by COVID-19"
+        />
+        <CardComponent
+          cardTitle="Vaccinated"
+          value={vaccine}
+          lastUpdate={vaccineLastUpdate}
+          cardSubtitle="Number of people have been vaccinated"
+        />
+      </Grid>
+    </div>
+  );
+};
+
+export default Cards;
