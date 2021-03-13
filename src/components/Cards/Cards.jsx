8b34fa8043179b78abc4e919ead25c7b6180e70d
calvary-k <karamoy.calvary@gmail.com>
2021-03-13 21:35:21 +0800

    feat: :lipstick: add styles for Cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import CardComponent from './Card/Card';
+import styles from './Cards.module.css';
 
 const Cards = ({ caseData: { confirmed, recovered, deaths, lastUpdate: caseLastUpdate },
   vaccineData: { vaccine, lastUpdate: vaccineLastUpdate }
@@ -11,27 +12,31 @@ const Cards = ({ caseData: { confirmed, recovered, deaths, lastUpdate: caseLastU
   }
 
   return (
-    <div>
+    <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <CardComponent
+          className={styles.confirmed}
           cardTitle="Infected"
           value={confirmed}
           lastUpdate={caseLastUpdate}
           cardSubtitle="Number of active cases of COVID-19"
         />
         <CardComponent
+          className={styles.recovered}
           cardTitle="Recovered"
           value={recovered}
           lastUpdate={caseLastUpdate}
           cardSubtitle="Number of recoveries from COVID-19"
         />
         <CardComponent
+          className={styles.deaths}
           cardTitle="Deaths"
           value={deaths}
           lastUpdate={caseLastUpdate}
           cardSubtitle="Number of deaths caused by COVID-19"
         />
         <CardComponent
+          className={styles.vaccine}
           cardTitle="Vaccinated"
           value={vaccine}
           lastUpdate={vaccineLastUpdate}
