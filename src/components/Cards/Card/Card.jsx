523f2f65b120f2a667738478a85b22af9a279c8e
calvary-k <karamoy.calvary@gmail.com>
2021-03-13 21:29:54 +0800

    feat: :lipstick: add styles for Card component

--- src/components/Cards/Card/Card.jsx
+++ src/components/Cards/Card/Card.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Card, CardContent, Grid, Typography } from '@material-ui/core';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
-const CardComponent = ({ cardTitle, value, lastUpdate, cardSubtitle }) => (
-  <Grid item xs={3} md={3} component={Card}>
+import styles from './Card.module.css';
+
+const CardComponent = ({ className, cardTitle, value, lastUpdate, cardSubtitle }) => (
+  <Grid item xs={12} md={3} component={Card} className={cx(styles.card, className)}>
     <CardContent>
       <Typography color="textSecondary" gutterBottom>{cardTitle}</Typography>
       <Typography variant="h5" component="h2">
