1be81357fd6c111d8db9b8c51b51233c377ecdc1
calvary-k <karamoy.calvary@gmail.com>
2021-03-13 19:45:00 +0800

    feat: :lipstick: add styles for App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './App.module.css';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <h1>App</h1>
       </div>
     );
