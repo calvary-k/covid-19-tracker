fd267a1e9a876f59cbb8895710a6f67bdf2f335e
calvary-k <karamoy.calvary@gmail.com>
2021-03-13 21:57:46 +0800

    feat: :lipstick: add styles for LocationPicker component

--- src/components/LocationPicker/LocationPicker.jsx
+++ src/components/LocationPicker/LocationPicker.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
 
 import { fetchProvincesName } from '../../api';
+import styles from './LocationPicker.module.css';
 
 const LocationPicker = ({ handleProvinceChange }) => {
   const [provinces, setProvinces] = useState([]);
@@ -15,7 +16,7 @@ const LocationPicker = ({ handleProvinceChange }) => {
   }, []);
 
   return (
-    <FormControl>
+    <FormControl className={styles.formControl}>
       <NativeSelect defaultValue="" onChange={(e) => handleProvinceChange(e.target.value)}>
         <option value="">INDONESIA</option>
         {provinces.map((province, i) => <option key={i} value={province}>{province}</option>)}
